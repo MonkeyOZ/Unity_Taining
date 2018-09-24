c2a91db802b8aa0806a78b2348cd067cb84a8490
Mortimer <monkeyoz@yeah.net>
2018-09-24 18:00:09 +0800

    第一个小练习———方块顶小球

---
+++ Demo/zhuangjixiaoqiu/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.GetComponent<Rigidbody>().velocity = new Vector3(-10.0f, 9.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
